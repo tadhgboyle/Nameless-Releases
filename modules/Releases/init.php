cd9c0d5a4464583e3180635fa24ca10e29120286
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-06-01 23:26:05 -0600

    require github token to load

--- modules/Releases/init.php
+++ modules/Releases/init.php
@@ -1,5 +1,10 @@
 <?php
 
+if (defined('BACK_END') && !Config::get('github_token')) {
+    // ok to error out since we can assume they're nmc staff
+    throw new RuntimeException('GitHub token is not set');
+}
+
 require_once(ROOT_PATH . '/modules/Releases/module.php');
 require_once(ROOT_PATH . '/modules/Releases/classes/Release.php');
 require_once(ROOT_PATH . '/modules/Releases/classes/GithubHelper.php');
